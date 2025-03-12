4d576767439f10dcf6fb1251cc470729f123fd7d
softqwak <valentin.kuzichev@yandex.ru>
2025-03-12 19:03:27 +0300

    готово, но ест один баг небольшой

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,98 +30,104 @@
         {
             this.txtWord = new System.Windows.Forms.TextBox();
             this.btnCheck = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.lblResult = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.tbxTransitions = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtWord
             // 
-            this.txtWord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWord.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWord.Location = new System.Drawing.Point(12, 12);
+            this.txtWord.Location = new System.Drawing.Point(101, 234);
             this.txtWord.Name = "txtWord";
-            this.txtWord.Size = new System.Drawing.Size(504, 26);
+            this.txtWord.Size = new System.Drawing.Size(274, 26);
             this.txtWord.TabIndex = 0;
             this.txtWord.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnCheck
             // 
-            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnCheck.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.btnCheck.Location = new System.Drawing.Point(12, 45);
+            this.btnCheck.Location = new System.Drawing.Point(101, 275);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(504, 32);
+            this.btnCheck.Size = new System.Drawing.Size(274, 33);
             this.btnCheck.TabIndex = 1;
-            this.btnCheck.Text = "check";
+            this.btnCheck.Text = "Проверить";
             this.btnCheck.UseVisualStyleBackColor = true;
             this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(8, 80);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Result: ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
             // lblResult
             // 
+            this.lblResult.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblResult.AutoSize = true;
-            this.lblResult.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblResult.Location = new System.Drawing.Point(86, 80);
+            this.lblResult.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblResult.Location = new System.Drawing.Point(97, 322);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(21, 20);
             this.lblResult.TabIndex = 3;
             this.lblResult.Text = "...";
             this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
             // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(9, 206);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(184, 48);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "alphabet: A B C D\r\nexamples of palindromes: \r\nABBA DDDD EDDE ABCCBA";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
             // tbxTransitions
             // 
-            this.tbxTransitions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxTransitions.AcceptsReturn = true;
+            this.tbxTransitions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tbxTransitions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.tbxTransitions.Location = new System.Drawing.Point(214, 119);
+            this.tbxTransitions.Location = new System.Drawing.Point(477, 0);
             this.tbxTransitions.Multiline = true;
             this.tbxTransitions.Name = "tbxTransitions";
-            this.tbxTransitions.ReadOnly = true;
-            this.tbxTransitions.Size = new System.Drawing.Size(302, 129);
+            this.tbxTransitions.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxTransitions.Size = new System.Drawing.Size(489, 617);
             this.tbxTransitions.TabIndex = 5;
+            this.tbxTransitions.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(98, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Слово";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(98, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 48);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "алфавит состоит из a b c d\r\nтолько четное количество букв\r\nпримеры полиндромов: a" +
+    "bba, dddd, aabbbaa";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 261);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(966, 616);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tbxTransitions);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.txtWord);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(544, 300);
+            this.MinimumSize = new System.Drawing.Size(955, 550);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -134,10 +140,10 @@
 
         private System.Windows.Forms.TextBox txtWord;
         private System.Windows.Forms.Button btnCheck;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblResult;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbxTransitions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,12 @@ namespace _2ritvv
     public partial class Form1 : Form
     {
 
-        private string currentState = "q0";
+        private string currentState = "Q0";
         private Stack<char> stack = new Stack<char>();
         private List<(string, string, Stack<char>)> transitions = new List<(string, string, Stack<char>)>();
         private string partWord = "";
         private bool isMiddleOfWord = false;
-
+        private bool isPolindrome = false;
         public Form1()
         {
             InitializeComponent();
@@ -43,76 +43,111 @@ namespace _2ritvv
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
-        private async void btnCheck_Click(object sender, EventArgs e)
-        {
-            tbxTransitions.Text = "";
-            if (txtWord.Text != "" && await isPolindromeCheck(txtWord.Text))
-            {
-
-            }
-            foreach(var item in transitions)
-            {
-                string magazine = "";
-                Stack<char> stack = new Stack<char>(item.Item3);
-                while (stack.Count != 0)
-                {
-                    magazine += stack.Pop();
-                }
-                tbxTransitions.Text += $"({item.Item1}, {item.Item2}, {magazine}Z0)\r\n";
-            }
-        }
-
         private void lblResult_Click(object sender, EventArgs e)
         {
 
         }
 
-        private async Task<bool> isPolindromeCheck(string word)
+        private async void btnCheck_Click(object sender, EventArgs e)
         {
-            await Task.Run(async () => {
-                for(int i = 0; i < word.Length - 1; i++)
-                {
-                    char symbol = word[i];
-                    string partWord = word.Substring(i);
-                    if (isMiddleOfWord)
-                    {
-                        // Логика после пройденой середины слова
+            tbxTransitions.Clear();
+            transitions.Clear();
+            stack.Clear();
+            isPolindrome = false;
 
-                    }
-                    else
-                    {
-                        // Логика до прохождения середины слова
-                        stack.Push(symbol); // Запись символов в 
-                        //if (stack != null)
-                            transitions.Add((currentState, partWord, new Stack<char>(stack)));
-                        await isMiddleOfWordCheck(partWord, stack); // Проверка на середину слова
-                    }
+            if (await isPolindromeCheck(txtWord.Text))
+            {
+                lblResult.Text = "Палиндром!";
+                lblResult.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblResult.Text = "Непалиндром!";
+                lblResult.ForeColor = Color.Red;
+            }
+            foreach (var item in transitions)
+            {
+                string magazine = null;
+                Stack<char> stack = new Stack<char>(item.Item3.Reverse());
+                while (stack.Count != 0)
+                {
+                    magazine += stack.Pop();
                 }
-            });
-            return false;
+                tbxTransitions.Text += $"|{item.Item1}, {(item.Item2.Any() ? item.Item2 : "ε")}, {magazine ?? "ε"} Z₀)\r\n";
+            }
         }
 
-        private async Task isMiddleOfWordCheck(string partWord, Stack<char> stack)
-        { 
+
+        private async Task<bool> isPolindromeCheck(string word)
+        {
+            await Task.Run(async () =>
+            {
+                transitions.Add((currentState, word, new Stack<char>(stack.Reverse())));
+                await isMiddleOfWordCheck(word, stack); // Проверка на середину слова
+                for (int i = 0; i <= word.Length - 1; i++)
+                {
+                    char symbol = word[i];
+                    string partWord = word.Substring(i + 1);
+                    stack.Push(symbol); // Запись символов в стэк
+                    transitions.Add((currentState, partWord, new Stack<char>(stack.Reverse())));
+                    await isMiddleOfWordCheck(partWord, stack); // Проверка на середину
+                }
+            });
+            if (isPolindrome == true && word.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async Task isMiddleOfWordCheck(string word, Stack<char> stack)
+        {
             await Task.Run(() =>
             {
-                foreach (char symbol in partWord)
+                Stack<char> tempStack = new Stack<char>(stack.Reverse());
+                int i = 0;
+                string partWord = "";
+                double x = 20;
+                for (; i <= word.Length - 1; i++)
                 {
-                    if (stack.Count == 0)
-                    {
-                        isMiddleOfWord = false;
-                        return;
-                    }
-                    char symbolForStack = stack.Pop();
+                    char symbol = word[i];
+                    partWord = word.Substring(i);
+                    transitions.Add(($"{"\\____ (Q1".PadLeft((int)x, ' ')}", partWord, new Stack<char>(tempStack.Reverse())));
+                    x += 20;
 
-                    if (symbolForStack != symbol)
+                    try
                     {
-                        isMiddleOfWord = false;
+                        char symbolForStack = tempStack.Pop();
+                        if (symbolForStack != symbol)
+                        {
+                            partWord = word.Substring(i + 1);
+                            transitions.Add(($"{"\\____ (Q2".PadLeft((int)x, ' ')}", partWord, new Stack<char>(tempStack.Reverse())));
+                            x += 20;
+                            return;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        transitions.Add(($"{"\\____ (Q2".PadLeft((int)x, ' ')}", partWord, new Stack<char>(tempStack.Reverse())));
+                        x += 20;
                         return;
                     }
                 }
+                partWord = word.Substring(i);
+                if (tempStack.Count == 0 && partWord.Length == 0)
+                    isPolindrome = true;
+                transitions.Add(($"{"\\____ (Q2".PadLeft((int)x, ' ')}", partWord, new Stack<char>(tempStack.Reverse())));
+                x += 20;
+
             });
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
